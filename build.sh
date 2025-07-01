4be3718a05df2cdd753485cc3fc9202e2b7fcac2
qkrwowoo <qkrwodn95@gmail.com>
2025-07-01 14:57:53 +0900

    pull path go binary

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ cd $1
 #go mod init $1
 #echo "require local/common v0.0.0" >> go.mod 
 #echo "replace local/common => ../common" >> go.mod 
-go mod tidy
-go build
+/usr/local/go/bin/go mod tidy
+/usr/local/go/bin/go build
