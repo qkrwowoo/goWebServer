28ffb8aa4e94976d900d74115983c0f4c66df91c
qkrwowoo <qkrwodn95@gmail.com>
2025-07-01 14:53:41 +0900

    do not go mod init

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd $1
-go mod init $1
-echo "require local/common v0.0.0" >> go.mod 
-echo "replace local/common => ../common" >> go.mod 
+#go mod init $1
+#echo "require local/common v0.0.0" >> go.mod 
+#echo "replace local/common => ../common" >> go.mod 
 go mod tidy
 go build
